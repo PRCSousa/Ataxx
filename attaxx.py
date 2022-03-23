b6271ada82a6160ad5cd1de93f5264e3fbb1b39e
TottyAmadeus <pedrosousa084@hotmail.com>
2022-03-23 22:32:04 +0000

    the damned select thing doesn't work, so we move to the next thing

--- attaxx.py
+++ attaxx.py
@@ -1,5 +1,4 @@
-from chess import Board
-from matplotlib.pyplot import draw
+
 import pygame as p
 
 # Defino o estado do jogo quando começa
@@ -23,26 +22,26 @@ class GameState():
 
         self.historico = []
 
-
-class Ataxx:
-
-    def __init__(self):
         self.jogador = True                  # True = Jogador Azul  / False = Jogador Vermelho
+
+        self.moves = [(-1, 1), (0, 1), (1, 1), (-1, 0),
+                      (1, 0), (-1, -1), (0, -1), (1, -1), 
+                      (-2, 2), (-2, 1), (-2, 0), (-2, -1), 
+                      (-2, -2), (-1, 2), (-1, -2),(0, 2), 
+                      (0, -2), (1, 2), (1, -2), (2, 2), 
+                      (2, 1), (2, 0), (2, -1), (2, -2)]
 
     def mudar_turno(self):
         # de cada vez que esta função é executada, o jogo muda de turno.
         self.jogador = not self.jogador
 
-    def get_all_possible_moves(self):
-        pos_copy = [(-1, 1), (0, 1), (1, 1), (-1, 0),
-                     (1, 0), (-1, -1), (0, -1), (1, -1)]
-        pos_jump = [(-2, 2), (-2, 1), (-2, 0), (-2, -1), (-2, -2), (-1, 2), (-1, -2),
-		(0, 2), (0, -2), (1, 2), (1, -2), (2, 2), (2, 1), (2, 0), (2, -1), (2, -2)]
+    
+
 
     ##########################################################################################
 
-
-p.init()  # Inicio um tabuleiro e defino o tamanho da janela, quantidade de quadrados, FPS
+#Variáveis Globais
+p.init()
 WIDTH = 600
 HEIGHT = 600
 DIMENSION = 7
@@ -51,36 +50,25 @@ SQ_SIZE = HEIGHT // DIMENSION
 IMAGES = {}
 
 # Ir buscar as imagens das peças do jogo
-
-
 def loadImages():
     pieces = ["1", "2", "3"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load(
             "images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
 
+
 # Desenhar o actual tabuleiro na janela
-
-
 def drawBoard(screen, board):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = p.Color(66, 179, 132)
             p.draw.rect(screen, color, p.Rect(
-                c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE - 2, SQ_SIZE - 2))
+            c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE - 2, SQ_SIZE - 2))
             if board[r][c] == "8":
-                p.draw.rect(screen, p.Color("gray"), p.Rect(
-                    c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE - 2, SQ_SIZE - 2))    
-
-
-def drawSelected(screen, sqrow, sqcol):
-            p.draw.rect(screen,p.Color("gray"), p.Rect(sqcol * SQ_SIZE, sqrow * SQ_SIZE, SQ_SIZE, SQ_SIZE))
-            print("foda-se")
-
+                 p.draw.rect(screen, p.Color("gray"), p.Rect(
+                     c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE - 2, SQ_SIZE - 2))    
 
 # Colocar as peças no tabuleiro
-
-
 def drawPieces(screen, board):
     for r in range(DIMENSION):
         for c in range(DIMENSION):
@@ -89,24 +77,38 @@ def drawPieces(screen, board):
                 screen.blit(IMAGES[piece], p.Rect(
                     c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
+# Desenha uma marca na peça carregada
+# def drawSelected(screen, board, primeiroclick):
+#     i = int(primeiroclick[0])
+#     j = int(primeiroclick[1])
+#     if board[j][i] == '8':
+#         return None
+#     elif board[j][i] == '1' or board[j][i] == '2':
+#             p.draw.rect(screen, p.Color(0,0,0), (j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE - 2, SQ_SIZE - 2))
+#             drawPieces(screen, board)  
+
+def mostrarmovimentos(screen, board, moves, primeiroclick):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            for move in range(len(moves)):
+                relativo = ()
+                relativo[0] = primeiroclick[0]-move[0]
+                relativo[1] = primeiroclick[1]-move[1]
+                if board[relativo[0]][relativo[1]] == '0':
+                    screen.blit(IMAGES[3], p.Rect(
+                    c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
 # Desenhar o estado do tabuleiro ditado pelo ataxxengine.py
-
-
 def drawGameState(screen, gs):
     drawBoard(screen, gs.board)
     drawPieces(screen, gs.board)
 
 
-
-# Função que trata do movimento das peças
-def moverpeca():
-    2
-
-
 def main():
     screen = p.display.set_mode((WIDTH, HEIGHT))  # Define o tamanho da janela
     clock = p.time.Clock()  # Faz o jogo correr no tempo
-    screen.fill(p.Color("black"))  # Fundo da janela preto
+    screen.fill((0,0,0)) #background branco  # Fundo da janela preto
     gs = GameState()  # Carrega o estado do jogo para uma variável
 #    print(gs.board)  # DEBUG: Dá print na consola do estado do jogo (TIRAR NO FIM)
     loadImages()  # Carrega as imagens
@@ -124,7 +126,6 @@ def main():
     # Variáveis usadas para o evento de clickar no tabuleiro
     qdselecionado = ()
     primeiroclick = ()
-
     while running:
 
         for e in p.event.get():
@@ -144,15 +145,12 @@ def main():
             # Verifica se é o primeiro clique ou o segundo
                 if primeiroclick == ():
                     primeiroclick = qdselecionado
-                    if gs.board[primeiroclick[0]][primeiroclick[1]] != '0' :
-                        if gs.board[primeiroclick[0]][primeiroclick[1]] != '8' :
-                            if gs.board[primeiroclick[0]][primeiroclick[1]] != '3' :
-                                sqrow = primeiroclick[0]
-                                sqcol = primeiroclick[1]
-                            drawSelected(screen, sqrow, sqcol)
+                            # drawSelected(screen,gs.board,primeiroclick)
                     qdselecionado = ()
 
             # At this point, temos de ver se carregamos num quadrado com uma peça
+                if gs.board[primeiroclick[0]][primeiroclick[1]] != '0' and gs.board[primeiroclick[0]][primeiroclick[1]] !='3' and gs.board[primeiroclick[0]][primeiroclick[1]] != '8' :
+                    mostrarmovimentos(screen, gs.board, gs.moves, primeiroclick)
 
                 #I'll work on this later ffs   
 
