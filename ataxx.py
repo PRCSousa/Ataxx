a0b01f842e065cb3b1dc457c445c625e61f8b08d
TottyAmadeus <pedrosousa084@hotmail.com>
2022-03-26 11:17:06 +0000

    possible moves working

--- ataxx.py
+++ ataxx.py
@@ -22,12 +22,12 @@ tabul = [
 tabcopia = []
 
 class movimento:
-    movimentoxi = 0
-    movimentoyi = 0
-    movimentoyf = 0
-    movimentojog = 0
-    movimentoxf = 0
-    movimentotipo = 0
+    xi = 0
+    yi = 0
+    yf = 0
+    jog = 0
+    xf = 0
+    tipo = 0
 
 
 def copia():
@@ -52,15 +52,15 @@ def outroJog(jog):
 def assinala_quad(x, y, atip,screen):
     if atip == 1:
         pygame.draw.ellipse(screen,(220, 20, 60),(y*sq+3,x*sq+3,6,6))
-        pygame.draw.ellipse(screen,(220, 20, 60),(y*sq+sq-9,x*sq+3,6,6))
-        pygame.draw.ellipse(screen,(220, 20, 60),(y*sq+3,x*sq+sq-9,6,6))
-        pygame.draw.ellipse(screen,(220, 20, 60),(y*sq+sq-9,x*sq+sq-9,6,6))
+        pygame.draw.ellipse(screen,(220, 20, 60),(y*sq+sq-11,x*sq+3,6,6))
+        pygame.draw.ellipse(screen,(220, 20, 60),(y*sq+3,x*sq+sq-11,6,6))
+        pygame.draw.ellipse(screen,(220, 20, 60),(y*sq+sq-11,x*sq+sq-11,6,6))
 
     elif atip == 2:
         pygame.draw.ellipse(screen,(106, 90, 205),(y*sq+3,x*sq+3,6,6))
-        pygame.draw.ellipse(screen,(106, 90, 205),(y*sq+sq-9,x*sq+3,6,6))
-        pygame.draw.ellipse(screen,(106, 90, 205),(y*sq+3,x*sq+sq-9,6,6))
-        pygame.draw.ellipse(screen,(106, 90, 205),(y*sq+sq-9,x*sq+sq-9,6,6))
+        pygame.draw.ellipse(screen,(106, 90, 205),(y*sq+sq-11,x*sq+3,6,6))
+        pygame.draw.ellipse(screen,(106, 90, 205),(y*sq+3,x*sq+sq-11,6,6))
+        pygame.draw.ellipse(screen,(106, 90, 205),(y*sq+sq-11,x*sq+sq-11,6,6))
 
 def mostra_tabul(screen, tabul):
     for r in range(N):
@@ -78,6 +78,38 @@ def mostra_tabul(screen, tabul):
                 pygame.draw.ellipse(screen, (106, 90, 205), pygame.Rect(
                     c * sq + (sq / 4), r * sq + (sq / 4), sq / 2, sq / 2))
 
+def multiplica():
+    dx = -1
+    dy = -1
+    for dx in range(dx, 1):
+        for dy in range(dy, 1):
+            if tabul[movimento.yf + dy][movimento.xf + dx] == outroJog(movimento.jog):
+                tabul[movimento.yf + dy][movimento.xf + dx] == movimento.jog
+
+def executa_movimento():
+    tabul[movimento.yf][movimento.xf] = movimento.jog
+    if movimento.tipo == 1:
+        [movimento.yi][movimento.xi] = 0
+    multiplica()        
+
+def adjacente(dist):
+    return(
+        abs(movimento.xi - movimento.xf) == dist and abs(movimento.yi - movimento.yf) <=dist or
+        abs(movimento.yi - movimento.yf) == dist and abs(movimento.xi - movimento.xf) <=dist
+        )
+
+def dentro(x,y):
+    return (x >= 0 and x <= N-1 and y >= 0 and y <= N-1)
+
+def movimento_valido():
+    if not dentro(movimento.xi, movimento.yi) or not dentro(movimento.xf, movimento.yf):
+        return False
+    if tabul[movimento.yi][movimento.xi] == movimento.jog and tabul[movimento.yf][movimento.xf] == 0 and adjacente(1):
+        movimento.tipo = 0
+        return True
+    if tabul[movimento.yi][movimento.xi] == movimento.jog and tabul[movimento.yf][movimento.xf] == 0 and adjacente(2):
+        movimento.tipo = 1
+        return True
 
 def tipo_jogo():
     print("Jogo de Ataxx")
@@ -88,18 +120,44 @@ def tipo_jogo():
     tipo = input()
     return tipo
 
+def jogadas_validas_pos(jog, yi,xi,tipoAss,screen):
+    nmovs = 0
+    if tabul[yi][xi] == jog:
+        for k in range(N):
+            for l in range(N):
+                movimento.jog = jog
+                movimento.yi = yi
+                movimento.xi = xi
+                movimento.yf = k
+                movimento.xf = l
+                if movimento_valido():
+                    assinala_quad(k,l,tipoAss,screen)
 
-def jogada_Humano(jog, xi, yi,screen):
-    cl = 0
-    if cl == 0 and tabul[yi][xi]==jog:
-        cl = 1
-        movimento.movimentoxi = xi
-        movimento.movimentoyi = yi
-        assinala_quad(yi, xi,1,screen)
+# def fim_jogo():
+
+def jogada_Humano(cl,jog, px, py,screen):
+    if cl == 0 and tabul[py][px]==jog:
+        movimento.xi = px
+        movimento.yi = py
+        assinala_quad(py, px,1,screen)
+        jogadas_validas_pos(jog,py,px,1,screen)
+    elif cl == 1:
+        movimento.xf = px
+        movimento.yf = py
+        assinala_quad(movimento.xf,movimento.yf1,0,screen)
+        if movimento_valido():
+            executa_movimento()
 
 
+
+def outroCl(cl):
+    if cl == 0:
+        return 1
+    else:
+        return 0
 
 def main():
+    cl = 0
     nMovs = 2
     jog = 0
     tipo = int(tipo_jogo())
@@ -126,12 +184,23 @@ def main():
                 xi = int(click[0] // sq)
                 if nMovs % 2 == 1:
                     if tipo <= 2:
-                        jogada_Humano(jog, xi, yi,screen)
+                        if cl == 0:
+                            jogada_Humano(cl,jog, xi, yi,screen)
+                            outroCl(cl)
+                        elif cl == 1:
+                            jogada_Humano(cl,jog, xi, yi,screen)
+                            outroCl(cl)
+                        
                     # else:
                     #     jogada_PC(jog, nMovs)
                 else:
                     if tipo == 1 or tipo == 3:
-                        jogada_Humano(jog, xi, yi,screen)
+                        if cl == 0:
+                            jogada_Humano(cl,jog, xi, yi,screen)
+                            outroCl(cl)
+                        elif cl == 1:
+                            jogada_Humano(cl,jog, xi, yi,screen)
+                            outroCl(cl)
                     # else:
                     #     jogada_PC(jog, nMovs)          
         pygame.display.flip()
